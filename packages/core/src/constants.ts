d8f77047d567e5d6be1a2efc7322a4e9c5ebd786
Peter Muriuki <pmuriuki98@gmail.com>
2022-10-07 12:35:11 +0300

    Rename core package

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,3 @@
+export const submittedDataEndpoint = 'api/v1/data' as const;
+export const formEndpoint = 'api/v1/forms' as const;
+export const editSumbissionEdnpoint = 'api/v1/submissions' as const;
--- packages/trasformer/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { LogMessageLevels } from "./types";
-
-export const createInfoLog = (message: string) => ({level: LogMessageLevels.INFO, message});
-export const createWarnLog = (message: string) => ({level: LogMessageLevels.WARN, message});
-export const createErrorLog = (message: string) => ({level: LogMessageLevels.ERROR, message});
-export const createDebugLog = (message: string) => ({level: LogMessageLevels.DEBUG, message});
-export const createVerboseLog = (message: string) => ({level: LogMessageLevels.VERBOSE, message});
