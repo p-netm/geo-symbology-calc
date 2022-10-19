e41e6dcadcceaf0a23ab166669b5536dfda9ddaa
Peter Muriuki <pmuriuki98@gmail.com>
2022-10-19 11:40:25 +0300

    Tweak styles on the landing page

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<p>
-	Geo Symbology Transform tool is a web based GUI that allows users to create configs and manually
-	trigger bulk evaluation of the marker-color of registered geo points when using Ona api
-</p>
-<p> Get started below by either:
-</p>
+<section class="jumbotron mx-auto home-banner mt-5 text-center">
+	<div class="">
+		<h1 class="jumbotron-heading">Geo Symbology Transform</h1>
+		<p class="lead">
+			Geo Symbology Transform tool is a web based GUI that allows users to create configs and
+			manually trigger bulk evaluation of the marker-color of registered geo points when using Ona
+			data api
+		</p>
+		<p class="">Get started by</p>
+		<div>
+			<a href="/configs" class="btn btn-outline-primary"> Create config</a>
+			<a href="/workflows" class="btn btn-outline-primary">See configured workflows</a>
+		</div>
+	</div>
+</section>
 
-<a href="/configs" class="btn btn-outline-primary"> Create config</a>
-<a href="/workflows" class="btn btn-outline-primary">See configured workflows</a>
+<style>
+	.home-banner {
+		width: 70%;
+	}
+</style>
